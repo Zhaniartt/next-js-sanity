393d3190e8c479fb2354aeb4878b1009120a21cb
Zhaniartt <zhaniartt@gmail.com>
2022-02-09 08:51:40 +0200

    Add header and home components

---
+++ pages/collections/[collectionId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const Collection = () => {
+    const router = useRouter();
+
+  return  <Link href="/">{router.query.collectionId}</Link>
+}
+
+export default Collection;
